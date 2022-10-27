237b3f37bb499a182b3a07b322cb0f3ad808eee4
EddyGlezz <eddyglezz95@gmail.com>
2022-10-27 12:45:57 +0200

    init repo

---
+++ FP2.cpp
@@ -0,0 +1,51 @@
+#include"FP2.hpp"
+namespace network {
+	namespace datasets{
+
+		FP2::FP2(const std::string& root, Mode mode): mode_(mode)
+		{
+			auto pathIM=(root+((mode==Mode::kTrain)?"/FP2_train_input.bin":"/FP2_test_input.bin"));
+			auto pathTA=(root+((mode==Mode::kTrain)?"/FP2_train_target.bin":"/FP2_test_target.bin"));
+			std::ifstream file_image((pathIM).c_str(),std::ios::in | std::ios::binary);
+			std::ifstream file_target((pathTA).c_str(),std::ios::in | std::ios::binary);
+			TORCH_CHECK(file_image, "Error opening data file at ", pathIM);
+			TORCH_CHECK(file_target, "Error opening data file at ", pathTA);
+			int32_t NitemsIM,maxEdges,NitemsTA;
+			file_image.read(reinterpret_cast<char *>(&NitemsIM), sizeof(NitemsIM));
+			file_target.read(reinterpret_cast<char *>(&NitemsTA), sizeof(NitemsTA));
+			file_image.read(reinterpret_cast<char *>(&maxEdges), sizeof(maxEdges));
+			std::cout<<"maxEdges:"<<(int)maxEdges<<std::endl;
+			std::cout<<"Nitems:"<<(int)NitemsTA<<" "<<(int)NitemsIM<<std::endl;
+			TORCH_CHECK(NitemsIM==NitemsTA,
+					"Number of examples in input and target do not match");
+
+			std::vector<int16_t> data_buffer_IM(NitemsIM*maxEdges);
+			std::for_each(data_buffer_IM.begin(),data_buffer_IM.end(),[&file_image](auto &item){
+					file_image.read(reinterpret_cast<char *>(&item), sizeof(item));
+					});
+
+
+			images_ = torch::from_blob(data_buffer_IM.data(),{NitemsIM,maxEdges},torch::dtype(torch::kInt16)).clone();
+			images_=images_.to(torch::kFloat64).div_(maxEdges);
+
+			std::vector<int8_t> data_buffer_TA(NitemsTA);
+			std::for_each(data_buffer_TA.begin(),data_buffer_TA.end(),[&file_target](auto &item){
+					file_target.read(reinterpret_cast<char *>(&item), sizeof(item));
+					});
+
+			targets_ = torch::from_blob(data_buffer_TA.data(),{NitemsTA},torch::dtype(torch::kInt8)).clone();
+file_image.close();
+file_target.close();
+
+		}
+		torch::data::Example<> FP2::get(size_t index)
+		{
+			return torch::data::Example(images_[index],targets_[index]);
+		}
+		c10::optional<size_t> FP2::size() const
+		{
+			return images_.size(0);
+		}
+
+	}
+}
---
+++ include/FP2.hpp
@@ -0,0 +1,44 @@
+
+#pragma once
+#include <ATen/ATen.h>
+#include <torch/torch.h>
+#include <iostream>
+#include<fstream>
+namespace network {
+	namespace datasets{
+		/**
+		 * Dataset based on the Featured Proposal [FP2](../../Proposals/FP_2.md)
+		 * Encoding the adjacency list as features.
+		 *
+		 */
+
+		class  FP2 : public torch::data::Dataset<FP2> {
+			public:
+				enum class Mode { kTrain, kTest };
+
+				explicit FP2(const std::string& root, Mode mode = Mode::kTrain);
+
+				torch::data::Example<> get(size_t index) override;
+
+				c10::optional<size_t> size() const override;
+
+				bool is_train() const noexcept{
+					return mode_ == Mode::kTrain;
+				}
+
+
+				const torch::Tensor& images() const{
+					return images_;
+				}
+
+				const torch::Tensor& targets() const{
+					return targets_;
+				}
+
+			private:
+				torch::Tensor images_, targets_;
+				Mode mode_;
+		};
+
+	};
+};
